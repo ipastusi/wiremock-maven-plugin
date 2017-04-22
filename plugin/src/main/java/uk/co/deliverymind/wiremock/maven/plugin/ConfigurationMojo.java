b7054480bfb9b47f5976b63dba40ecc7e0cc33bb
Tjisse van der Wal <tjisse.van.der.wal@ing.nl>
2017-04-23 01:33:54 +0200

    Add plugin parameter for setting Wiremock extensions and make the use of extensions from Maven classpath possible

--- plugin/src/main/java/uk/co/deliverymind/wiremock/maven/plugin/ConfigurationMojo.java
+++ plugin/src/main/java/uk/co/deliverymind/wiremock/maven/plugin/ConfigurationMojo.java
@@ -4,6 +4,7 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.File;
+import java.util.List;
 
 abstract public class ConfigurationMojo extends AbstractMojo {
 
@@ -15,6 +16,12 @@ abstract public class ConfigurationMojo extends AbstractMojo {
     private File dir;
 
     /**
+     * Set extension classes to be loaded (<i>--extensions</i>), for example custom matchers or response transformers.
+     */
+    @Parameter(property = "extensions")
+    private List<String> extensions;
+
+    /**
      * Set all other parameters in command-line format, e.g.:
      * <i>--port=8081 --verbose</i>
      * Do <b>NOT</b> specify <i>--root-dir</i> here.
@@ -23,7 +30,19 @@ abstract public class ConfigurationMojo extends AbstractMojo {
     private String params;
 
     protected String[] getAllParams() {
-        String allParams = String.format("%s%s %s", "--root-dir=", dir, params);
+        String extensionParam = "";
+        if (extensions != null && !extensions.isEmpty()) {
+            extensionParam = String.format(" %s%s", "--extensions=", join(",", extensions));
+        }
+        String allParams = String.format("%s%s%s %s", "--root-dir=", dir, extensionParam, params);
         return allParams.split(" ");
+    }
+
+    private String join(String separator, List<String> items) {
+        StringBuilder b = new StringBuilder();
+        for (String item: items) {
+            b.append(separator).append(item);
+        }
+        return items.isEmpty() ? "" : b.toString().substring(separator.length());
     }
 }
--- plugin/src/main/java/uk/co/deliverymind/wiremock/maven/plugin/WireMockMojo.java
+++ plugin/src/main/java/uk/co/deliverymind/wiremock/maven/plugin/WireMockMojo.java
@@ -2,17 +2,47 @@ package uk.co.deliverymind.wiremock.maven.plugin;
 
 import com.github.tomakehurst.wiremock.standalone.WireMockServerRunner;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.descriptor.PluginDescriptor;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.codehaus.plexus.classworlds.realm.ClassRealm;
 
+import java.io.File;
+import java.net.MalformedURLException;
 import java.util.Arrays;
+import java.util.List;
 
-@Mojo(name = "run", defaultPhase = LifecyclePhase.PRE_INTEGRATION_TEST)
+@Mojo(name = "run", defaultPhase = LifecyclePhase.PRE_INTEGRATION_TEST, requiresDependencyResolution = ResolutionScope.TEST)
 public class WireMockMojo extends ConfigurationMojo {
 
+    @Parameter(defaultValue = "${plugin}", required = true, readonly = true)
+    private PluginDescriptor descriptor;
+
+    @Parameter(property = "project.testClasspathElements", required = true, readonly = true)
+    private List<String> classpathElements;
+
     public void execute() throws MojoExecutionException {
+        loadTestClasspath();
+
         String wireMockParams = Arrays.toString(getAllParams()).replaceAll("[\\[\\]]", "").replaceAll(", ", " ");
         getLog().info("Starting WireMock with following params: " + wireMockParams);
         WireMockServerRunner.main(getAllParams());
+    }
+
+    private void loadTestClasspath() throws MojoExecutionException {
+        ClassRealm realm = descriptor.getClassRealm();
+
+        for (String element : classpathElements)
+        {
+            getLog().debug(String.format("Adding %s to Wiremock plugin classpath", element));
+            File elementFile = new File(element);
+            try {
+                realm.addURL(elementFile.toURI().toURL());
+            } catch (MalformedURLException e) {
+                throw new MojoExecutionException("Malformed classpath URL", e);
+            }
+        }
     }
 }

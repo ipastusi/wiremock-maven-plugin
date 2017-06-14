3c44a53a4a9be917d9e4d54e5b03091463f3a6b8
Irek Pastusiak <irek@deliverymind.co.uk>
2017-06-14 12:06:25 +0100

    Add JobDSL project definition

---
+++ jobdsl.groovy
@@ -0,0 +1,49 @@
+#!groovy
+
+folder('wiremock-maven-plugin-tmp')
+
+pipelineJob('wiremock-maven-plugin-tmp/build') {
+    concurrentBuild(false)
+    properties {
+        buildDiscarder {
+            strategy {
+                logRotator {
+                    numToKeepStr('10')
+                    daysToKeepStr(null)
+                    artifactDaysToKeepStr(null)
+                    artifactNumToKeepStr(null)
+                }
+            }
+        }
+        githubProjectProperty {
+            projectUrlStr('https://github.com/deliverymind/wiremock-maven-plugin/')
+        }
+        rebuildSettings {
+            autoRebuild(true)
+            rebuildDisabled(false)
+        }
+    }
+    parameters {
+        stringParam('RELEASE_VERSION', '3.0.0', '3.1.0, 3.2.0 etc')
+        stringParam('POST_RELEASE_SNAPSHOT_VERSION', '3.0.1-SNAPSHOT', '3.1.1-SNAPSHOT, 3.2.1-SNAPSHOT etc')
+        nonStoredPasswordParam('GPG_PASSPHRASE', 'GPG passphrase for signing artefacts')
+        stringParam('REPO_URL', 'git@github.com:deliverymind/wiremock-maven-plugin.git', 'Or local path, e.g.: "file:///Users/username/git/wiremock-maven-plugin"')
+        stringParam('GIT_CREDENTIALS_ID', '11be7a79-8034-407b-8351-dbd1d3342c24', '')
+        stringParam('TEST_ONLY', 'true', '')
+        stringParam('DRY_RUN', 'true', '')
+    }
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    branch('*/master')
+                    remote {
+                        credentials('11be7a79-8034-407b-8351-dbd1d3342c24')
+                        url('${REPO_URL}')
+                    }
+                }
+            }
+            scriptPath('Jenkinsfile')
+        }
+    }
+}

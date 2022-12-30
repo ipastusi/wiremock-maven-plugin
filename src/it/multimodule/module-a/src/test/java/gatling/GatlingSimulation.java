b19b52f67f06b4daac2b5a398655e3e846847b16
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2022-12-30 10:59:47 +0000

    Move jmeter -> gatling

---
+++ src/it/multimodule/module-a/src/test/java/gatling/GatlingSimulation.java
@@ -0,0 +1,31 @@
+package gatling;
+
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.HttpProtocolBuilder;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.http;
+import static io.gatling.javaapi.http.HttpDsl.status;
+
+public class GatlingSimulation extends Simulation {
+
+    private final HttpProtocolBuilder httpProtocol = http.baseUrl("http://localhost:8080");
+    private final ScenarioBuilder scenario = scenario("get")
+            .exec(http("get")
+                    .get("/healthcheck")
+                    .check(
+                            status().is(200),
+                            substring("wiremock-maven-plugin-a")
+                    )
+            );
+
+    {
+        setUp(scenario.injectClosed(constantConcurrentUsers(1).during(10)))
+                .maxDuration(30)
+                .protocols(httpProtocol)
+                .assertions(
+                        global().successfulRequests().percent().is(100.0)
+                );
+    }
+}
